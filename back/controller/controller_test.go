c89f368aa1e803965e60919e4311f1a4a2d074ed
Luis Ricardo Espinoza Rodriguez <ricardo.espinoza@ricardo.espinoza>
2021-03-29 10:36:36 -0600

    added unit tests to controller.go

--- back/controller/controller_test.go
+++ back/controller/controller_test.go
@@ -1,1 +1,50 @@
 package controller
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/ricardope3/academy-go-q12021/back/controller/mocks"
+	"github.com/ricardope3/academy-go-q12021/back/models"
+	"github.com/stretchr/testify/assert"
+)
+
+//mockgen -source=controller/controller.go -destination=controller/mocks/controller.go -package=mocks
+func Test_Controller(t *testing.T) {
+
+	tests := []struct {
+		name                    string
+		expectedParams          []models.Todo
+		expectedUsecaseResponse int
+		expectUsecaseCall       bool
+		expectedError           error
+		wantError               bool
+	}{
+		{
+			name:                    "OK, save CSV",
+			expectedParams:          []models.Todo{},
+			expectedUsecaseResponse: 200,
+			expectUsecaseCall:       true,
+			wantError:               false,
+			expectedError:           nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			u := mocks.NewMockUseCase(mockCtrl)
+
+			if tt.expectUsecaseCall {
+				u.EXPECT().SaveCSV(tt.expectedParams).Return(tt.expectedUsecaseResponse)
+			}
+
+			c := New(u)
+
+			response := c.useCase.SaveCSV(tt.expectedParams)
+			assert.Equal(t, response, tt.expectedUsecaseResponse)
+
+		})
+	}
+}
--- back/go.mod
+++ back/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/mock v1.5.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/unrolled/render v1.0.3 // indirect
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
